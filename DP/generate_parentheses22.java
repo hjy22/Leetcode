7f876272026d1ebc2a4b6dbeaf50f6977415969d
hjyy22 <hjyyyyy22@gmail.com>
2023-10-18 15:57:12 -0700

    dp_proble

---
+++ DP/generate_parentheses22.java
@@ -0,0 +1,40 @@
+package DP;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class generate_parentheses22 {
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(generateParenthesis(3));
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        Set<String> set = new HashSet<>();
+        set.add("()");
+        for(int i=1;i<n;i++){
+            Set<String> setResult = new HashSet<>();
+            for(String setItem: set){
+                for(int j=0;j<=setItem.length();j++){
+                    setResult.add(buildString(setItem,j));
+                }
+            }
+            set = setResult;
+        }
+        return convertSetToList(set);
+    }
+
+    static String buildString(String str, int index){
+        return str.substring(0,index)+"()"+str.substring(index);
+    }
+
+    static List<String> convertSetToList(Set<String> set){
+        List<String> array = new ArrayList<>();
+        for(String setItem: set){
+            array.add(setItem);
+        }
+        return array;
+    }
+}
