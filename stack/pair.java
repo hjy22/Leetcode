4ceab80e93a8e9d453e6333d3396c0a94cb99cc2
hjyy22 <hjyyyyy22@gmail.com>
2023-10-19 16:02:42 -0700

    pair

---
+++ stack/pair.java
@@ -0,0 +1,36 @@
+package stack;
+
+import java.util.Stack;
+
+public class pair {
+    public static void main(String[] args) {
+        String s = "[[[]]]{{{}}}((())))";
+        System.out.println(isBalanced(s));
+    }
+
+    static boolean isBalanced(String s){
+    if(s.length()==0){
+        return true;
+    }
+    Stack<Character> stack = new Stack();
+    char[] array = s.toCharArray();
+    for(Character c:array){
+        if(stack.isEmpty()){
+            return false;
+        }
+        if(c=='('||c=='{'||c=='['){
+            stack.push(c);
+        }else if(c==')'&&stack.peek()=='('){
+            stack.pop();
+        }else if(c=='}'&&stack.peek()=='{'){
+            stack.pop();
+        }else if(c==']'&&stack.peek()=='['){
+            stack.pop();
+        }
+    }
+    if(stack.isEmpty()){
+        return true;
+    }
+    return false;
+    }
+}
